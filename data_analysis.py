63cb2f10980fc6761fe2729ff6c11bcc4d06260c
Hanrui Zhang <rayezh@zeeb.dcmb.med.umich.edu>
2020-09-04 14:41:09 -0400

    update

--- data_analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from collections import Counter
-for i in range(10):
-    p = pd.read_csv('./invivo/fold_'+str(i)+'/Train.csv')
-    print('fold_',i)
-    print(dict(Counter(p['Kmeans.Grp'])))
-    print(dict(Counter(p['Country'])))
-    print(dict(Counter(p['Asexual.stage..hpi.'])))
-    #print(dict(Counter(p['ClearanceRate'])))
